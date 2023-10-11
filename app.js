b63b2ee601a129d61f2d62c1241a5e20ff780bcf
Mahdi <amiralmasi021@gmail.com>
2023-10-11 18:28:22 +0330

    Create server and learn get and post

--- app.js
+++ app.js
@@ -2,8 +2,13 @@ const express = require('express')
 
 const app = express();
 
-
-
+app.get('/' , (req , res) => {
+    res.status(404).json
+    ({message : ' hello from the serve side!' , app:'Natours'})
+} )
+app.post('/', (req,res ) => {
+    res.send("you can post")
+})
 
 app.listen(8000, () =>{
     console.log("Listening ... on port 8000")
