5dc0382e520450692fc8d1c000db036c82497178
Mahdi <amiralmasi021@gmail.com>
2023-10-12 14:52:10 +0330

    Exercise with params

--- app.js
+++ app.js
@@ -9,7 +9,17 @@ app.use(express.json())
 const tours = JSON.parse(fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`))
 
 
-app.get('/api/v1/tours' , (req , res) => {
+app.get('/api/v1/tours/:id' , (req , res) => {
+
+    const id = req.params.id * 1 ;
+    const tour = tours.find(el => el.id = id)  
+    if(id > tours.length){
+    // if(!tour){
+        return res.status(404).json({status:'fail' , message : 'Invalid ID'})
+    }
+
+
+
     res.status(200).json({
         status : 'success',
         data: {
@@ -19,8 +29,19 @@ app.get('/api/v1/tours' , (req , res) => {
 } )
 
 app.post('/api/v1/tours', (req,res ) => {
-    console.log(req.body)
-    res.send('Done')
+    // console.log(req.body)
+
+    const newId = tours[tours.length - 1].id + 1
+    const newTour  = Object.assign({id:newId} , req.body)
+    tours.push(newTour)
+    fs.writeFile(`${__dirname}/dev-data/data/tours-simple.json`, JSON.stringify(tours) ,err =>{
+        res.status(201).json({
+            status:'success',
+            data:{
+                tours : newTour  
+            }
+        })
+    })
 })
 
 app.listen(8000, () =>{
