d9e70c1d178f8c53cabc3bf23aa4779a94736655
Mahdi <amiralmasi021@gmail.com>
2023-10-12 19:00:24 +0330

    Add delete and patch requests

--- app.js
+++ app.js
@@ -1,25 +1,16 @@
 const express = require('express')
 const fs = require('fs')
-
 const app = express();
-
 // middleware
 app.use(express.json())
-
 const tours = JSON.parse(fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`))
-
-
 app.get('/api/v1/tours/:id' , (req , res) => {
-
     const id = req.params.id * 1 ;
     const tour = tours.find(el => el.id = id)  
     if(id > tours.length){
     // if(!tour){
         return res.status(404).json({status:'fail' , message : 'Invalid ID'})
     }
-
-
-
     res.status(200).json({
         status : 'success',
         data: {
@@ -27,10 +18,8 @@ app.get('/api/v1/tours/:id' , (req , res) => {
         }
     })
 } )
-
 app.post('/api/v1/tours', (req,res ) => {
     // console.log(req.body)
-
     const newId = tours[tours.length - 1].id + 1
     const newTour  = Object.assign({id:newId} , req.body)
     tours.push(newTour)
@@ -43,6 +32,32 @@ app.post('/api/v1/tours', (req,res ) => {
         })
     })
 })
+
+app.patch('/api/v1/tour/:id' , (req , res) =>{
+    if(req.param.id*1 > tours.length){
+        // if(!tour){
+            return res.status(404).json({status:'fail' , message : 'Invalid ID'})
+        }
+    res.status(200).json({
+        status:'success',  
+        data:{
+            tours : '<Updated here>'
+        }
+    })
+})
+app.delete('/api/v1/tour/:id' , (req , res) =>{
+    if(req.param.id*1 > tours.length){
+        // if(!tour){
+            return res.status(404).json({status:'fail' , message : 'Invalid ID'})
+        }
+    res.status(202).json({
+        status:'success',
+        data:{
+            tours : null
+        }
+    })
+})
+
 
 app.listen(8000, () =>{
     console.log("Listening ... on port 8000")
