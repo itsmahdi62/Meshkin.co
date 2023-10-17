0a31a06b9412b5019c367ffe2944332158bc55bb
Mahdi <amiralmasi021@gmail.com>
2023-10-17 19:31:42 +0330

    test

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
 const morgan = require('morgan')
-const tourRouter = require('./routes/tourRoutes')
-const userRouter = require('./routes/userRoutes.js')
-const mongoose = require('mongoose')
-const dotenv = require('dotenv')
 const express = require('express')
 const app = express();
+const tourRouter = require('./routes/tourRoutes')
+const userRoutes = require('./routes/userRoutes.js')
+const mongoose = require('mongoose')
+const dotenv = require('dotenv')
 dotenv.config({path: './config.env'})
 // const DB = process.env.DATABASE.replace('<PASSWORD>' ,process.env.DATABASE_PASSWORD)
 
@@ -23,11 +23,19 @@ if(process.env.NODE_ENV === 'development'){
     app.use(morgan('dev'))
 } 
 app.use(express.json())
+app.use(morgan('dev'))
 
 app.use((req,res,next) =>{
     console.log("The middleware");
     next()
 })
+app.use((req,res,next) =>{
+    req.requestTime = new Date().toISOString();
+    next();
+})
+
+
+
 
 app.use('/api/v1/tours' , tourRouter)
 app.use('/api/v1/user' , userRouter)
--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -2,9 +2,7 @@ const express = require('express')
 const router = express.Router();
 const tourController = require('../controllers/tourController')
 
-router.param('id' , tourController.checkID )
-
-router.route('/').get(tourController.getAllTour).post( tourController.checkPatch ,tourController.createTour)
+router.route('/').get(tourController.getAllTour).post(tourController.createTour)
 router.route('/:id').get(tourController.getTour).patch(tourController.patchTour).delete(tourController.deleteTour);
 
 module.exports = router
