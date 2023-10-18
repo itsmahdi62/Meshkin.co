5f556e10483d6be96c99dc00b59d3f0ef371d3be
Mahdi <amiralmasi021@gmail.com>
2023-10-18 20:35:22 +0330

    Connect to local database and create first mongoose chema model

--- app.js
+++ app.js
@@ -12,10 +12,36 @@ mongoose.connect(process.env.DATABASE_LOCAL , {
     useNewUrlParser:true ,
     useCreateIndex :true ,
     useFindAndModify: false
-}).then(con => {
-    console.log(con.connections);
+}).then(() => {
+    // console.log(con.connections);
     console.log('DB connection successful')
 })
+
+const tourSchema  =new mongoose.Schema({
+    name : {
+        type:String,
+        required: [true , 'A tour must has a name !'],
+        unique : true
+    },
+    rarting : {
+        type : Number ,
+        required : [ true , 'A tour must has a rating' ],
+        default : 4.5
+    } , 
+    price:Number ,
+})
+const Tour = mongoose.model('Tour' , tourSchema);
+
+const testTour =  new Tour({
+    name : 'The forest hiker',
+    rating: 4.7,
+    price:497
+})
+
+testTour.save().then(doc =>{
+    console.log(doc);
+}).catch(err => console.log('Error 😘 '))
+
 
 const app = express();
 // middleware
@@ -30,10 +56,6 @@ app.use((req,res,next) =>{
     req.requestTime = new Date().toISOString();
     next();
 })
-
-// console.log(dotenv)
-
-
 app.use('/api/v1/tours' , tourRouter)
 app.use('/api/v1/user' , userRouter)
 
