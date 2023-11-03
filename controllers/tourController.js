72c68ac45f5aa581a28ec4a17b67f87409defff4
Mahdi <amiralmasi021@gmail.com>
2023-11-03 22:22:29 +0330

    Virtual properties

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -169,7 +169,12 @@ exports.getTourStats = async (req,res) => {
                     numRatings: {$sum : '$ratingsQuantity'}
                 }
             },
-            
+            {
+                $sort: {avgPrice:1}
+            },
+            // {
+            //     $match : { _id: { $ne: 'EASY'} }
+            // }
         ])
         res.status(201).json({
             status:'success',
@@ -189,11 +194,24 @@ exports.getTourStats = async (req,res) => {
 exports.getMonthlyPlan = async (req,res) =>{
     try{
         const year = req.params.year * 1;
-
         const plan = await Tour.aggregate([
             {
                 $unwind: '$startDates'
-            }
+            },
+            {
+                $match:{
+                    startDates:{
+                        $gte: new Date(`${year}-01-01`),
+                        $lte: new Date(`${year}-12-31`)
+                    }
+                }
+            },
+            {
+                $group:{
+                    _id: {$month: '$startDates'},
+                    numTourStarts: {$sum:1}
+                }
+            },
         ])
         res.status(200).json({
             status : 'success',
--- models/tourModel.js
+++ models/tourModel.js
@@ -51,6 +51,13 @@ const tourSchema  = new mongoose.Schema({
     },
     startDates:[Date]
 
+},{
+    toJSON:{virtuals:true},
+    toObject:{virtuals:true},
+})
+
+tourSchema.virtual('durationWeeks').get(function(){
+    return this.duration / 7 ;
 })
 
 const Tour = mongoose.model('Tour' , tourSchema);
