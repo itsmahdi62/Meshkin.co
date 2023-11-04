59eb5bb46156aa1d144a392b66f781215865ba30
Mahdi <amiralmasi021@gmail.com>
2023-11-04 22:11:04 +0330

    Create User Schema

---
+++ models/userModel.js
@@ -0,0 +1,33 @@
+const mongoose = require('mongoose')
+const validator = require('validator')
+// name , email , photo , password , passwordConfirm
+
+const userSchema = new mongoose.Schema({
+    name:{
+        type:String,
+        required : [true , "A user must have a name !"],
+    },
+    email:{
+        type:String , 
+        required:[true , "please provide your email"],
+        unique:true , 
+        lowercase:true,
+        validate : [validator.isEmai , 'Please provide a valid email']
+    },
+    photo:String,
+    password: {
+        type:String , 
+        required: [true , "Please provide a password"],
+        minlength:8
+    },
+    passwordConfirm:{
+        type:String , 
+        required: [true , 'Please confirm your password']
+    }
+})
+
+
+
+const User = mongoose.model('User' , tourSchema);
+
+module.exports  = User
