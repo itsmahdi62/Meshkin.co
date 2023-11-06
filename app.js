c5d75cfbba5c3cd7425be2dab5ea55ccb0810466
Mahdi <amiralmasi021@gmail.com>
2023-11-06 23:26:52 +0330

    Add default error handler

--- app.js
+++ app.js
@@ -32,9 +32,23 @@ app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
 
 app.all('*', (req, res, next) => {
-  res.status(404).json({
-    status: 'fail',
-    message: `Can not find ${req.originalUrl} on this server`,
+  // res.status(404).json({
+  //   status: 'fail',
+  //   message: `Can not find ${req.originalUrl} on this server`,
+  // });
+  const err = new Error(`Can not find ${req.originalUrl} on this server`);
+  err.status = 'fail' 
+  err.statusCode = 404;
+  next(err);
+});
+
+app.use((err, req, res, next) => {
+  err.statusCode = err.statusCode || 500; // internal server error
+  err.status = err.status || 'error';
+
+  res.status(err.statusCode).json({
+    status: err.status,
+    message: err.message,
   });
 });
 
