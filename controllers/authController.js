a963193a8fd0badee014d753f3e707f73eae1cf8
Mahdi <amiralmasi021@gmail.com>
2023-11-15 18:22:01 +0330

    Create first JWT

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,27 +1,26 @@
 const User = require('../models/userModel');
 const catchAsync = require('../utils/catchAsync');
+const jwt = require('jsonwebtoken')
 
-exports.signup = async (req, res, next) => {
-  try {
-    const newUser = await User.create({
-      name:req.body.name,
-      email:req.body.email,
-      password:req.body.password,
-      passwordConfirm:req.body.passwordConfirm
-    })
+exports.signup = catchAsync( async (req, res, next) => {
+  
+  const newUser = await User.create({
+    name:req.body.name,
+    email:req.body.email,
+    password:req.body.password,
+    passwordConfirm:req.body.passwordConfirm
+  })
 
-    res.status(201).json({
-      status: 'success',
-      data: {
-        user: newUser,
-      },
-    });
-  } catch (err) {
-    res.status(404).json({
-      status: 'fail',
-      data: {
-        err,
-      },
-    });
-  }
-};
+  const token = jwt.sign({ id : newUser._id } , process.env.JWT_SECRET , {
+    expiresIn: process.env.JWT_EXPIRES_IN
+  })
+
+  res.status(201).json({
+    status: 'success',
+    token,
+    data: {
+      user: newUser,
+    },
+  });
+
+})
