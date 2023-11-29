2f2e49e4cffc1259e924deb135af76e521728737
Mahdi <amiralmasi021@gmail.com>
2023-11-29 20:49:20 +0330

    Debug the code and send mail to user

--- utils/email.js
+++ utils/email.js
@@ -3,7 +3,7 @@ const nodemailer = require('nodemailer');
 const sendEmail = async (options) => {
   // 1) Create a transporter
   const transporter = nodemailer.createTransport({
-    service: process.env.EMAIL_HOST,
+    host: process.env.EMAIL_HOST,
     port: process.env.EMAIL_PORT,
     auth: {
       user: process.env.EMAIL_USERNAME,
