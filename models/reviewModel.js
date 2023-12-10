e9fd4b74bc7e1e6275ccba7409d57998d649c8f6
Mahdi <amiralmasi021@gmail.com>
2023-12-10 23:10:38 +0330

    Use parent refrencing for review user and tour

--- models/reviewModel.js
+++ models/reviewModel.js
@@ -8,28 +8,28 @@ const reviewSchema = new mongoose.Schema(
     },
     rating: {
       type: Number,
-      min:1 ,
-      max:5
+      min: 1,
+      max: 5,
     },
     createdAt: {
       type: Date,
-      default:Date.now(),
+      default: Date.now(),
       required: true,
     },
     tours: {
-        type : mongoose.Schema.ObjectId,
-        ref: 'Tour',
-        required : [true , 'Review must belong to a tour.']
+      type: mongoose.Schema.ObjectId,
+      ref: "Tour",
+      required: [true, "Review must belong to a tour."],
     },
-    user:{
-        type : mongoose.Schema.ObjectId,
-        ref: 'User',
-        required : [true , 'Review must belong to a User.']
-    }
+    user: {
+      type: mongoose.Schema.ObjectId,
+      ref: "User",
+      required: [true, "Review must belong to a User."],
+    },
   },
   {
-    toJSON: {virtuals: true},
-    toObject:{virtuals : true}
+    toJSON: { virtuals: true },
+    toObject: { virtuals: true },
   }
 );
 
