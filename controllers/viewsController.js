48ac9ef679baec5deec924daf5a8e062354fc1d4
Mahdi <amiralmasi021@gmail.com>
2023-12-17 14:13:14 +0330

    Create dynamic home page

--- controllers/viewsController.js
+++ controllers/viewsController.js
@@ -1,6 +1,14 @@
-exports.getOverview = (req, res) => {
+const Tour = require("../models/tourModel");
+const catchAsync = require("../utils/catchAsync");
+exports.getOverview = async (req, res) => {
+  // 1) Get tour data from collection
+  const tours = await Tour.find();
+  // 2) build template
+
+  // 3)Render that template using tour data from
   res.status(200).render("overview", {
     title: "All Tours",
+    tours
   });
 };
 
