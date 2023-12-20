8f1c1758d199505b1c781779e36bf806f63b19a4
Mahdi <amiralmasi021@gmail.com>
2023-12-20 19:28:27 +0330

    Create Login page

--- controllers/viewsController.js
+++ controllers/viewsController.js
@@ -8,12 +8,27 @@ exports.getOverview = async (req, res) => {
   // 3)Render that template using tour data from
   res.status(200).render("overview", {
     title: "All Tours",
-    tours
+    tours,
   });
 };
 
-exports.getTour = (req, res) => {
+exports.getTour = async (req, res, next) => {
+  // 1) GET THE data , for the requested route
+  const tour = await Tour.findOne({ slug: req.params.slug }).populate({
+    path: "reviews",
+    fields: "review rating user",
+  });
+  // 2) Build template
+
+  // 3) Render template using data from 1)
   res.status(200).render("tour", {
-    title: "The forest hiker",
+    title: `${tour.name} Tour`,
+    tour,
+  });
+};
+
+exports.getLoginForm = async (req, res) => {
+  res.status(200).render("login", {
+    title: "Log into your account",
   });
 };
--- routes/viewRoutes.js
+++ routes/viewRoutes.js
@@ -4,6 +4,8 @@ const router = express.Router();
 
 router.get("/", viewsController.getOverview);
 
-router.get("/tour", viewsController.getTour);
+router.get("/tour/:slug", viewsController.getTour);
+
+router.get('/login' , viewsController.getLoginForm)
 
 module.exports = router;
