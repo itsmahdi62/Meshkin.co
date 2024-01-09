5d7b0f123a85238dfb45c2099e37f6af84ea50b3
Mahdi <amiralmasi021@gmail.com>
2024-01-09 22:48:47 +0330

    Add react router loader

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -2,7 +2,7 @@
 import "./App.css";
 import Home from "./ui/Home";
 import Cart from "./features/cart/Cart";
-import Menu from "./features/Menu/Menu";
+import Menu, { loader as menuLoader } from "./features/Menu/Menu";
 import CreateOrder from "./features/order/CreateOrder";
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import Order from "./features/order/Order";
@@ -18,6 +18,7 @@ const router = createBrowserRouter([
       {
         path: "/menu",
         element: <Menu />,
+        loader: menuLoader,
       },
       {
         path: "cart",
