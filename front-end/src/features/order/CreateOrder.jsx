7a4ebe5febfd3517d6581b148e80d6a304958c14
Mahdi <amiralmasi021@gmail.com>
2024-01-16 23:53:48 +0330

    Add some tailwind classes

--- front-end/src/features/order/CreateOrder.jsx
+++ front-end/src/features/order/CreateOrder.jsx
@@ -42,28 +42,37 @@ function CreateOrder() {
   const cart = fakeCart;
 
   return (
-    <div>
-      <h2>Ready to order? Let's go!</h2>
+    <div className="px-4 py-6">
+      <h2 className="mb-8 text-xl font-semibold">Ready to order? Let's go!</h2>
 
       {/* <Form method="POST" action="/order/new"> */}
       <Form method="POST">
-        <div>
-          <label>First Name</label>
-          <input type="text" name="customer" required className="input" />
+        <div className="mb-5 flex flex-col gap-2 sm:flex-row sm:items-center">
+          <label className="sm:basis-40">First Name</label>
+          <input className="input grow" type="text" name="customer" required />
         </div>
 
-        <div>
-          <label>Phone number</label>
-          <div>
-            <input type="tel" name="phone" required className="input" />
+        <div className="mb-5 flex flex-col gap-2 sm:flex-row sm:items-center">
+          <label className="sm:basis-40">Phone number</label>
+          <div className="grow">
+            <input type="tel" name="phone" required className="input w-full" />
+            {formErrors?.phone && (
+              <p className="mt-2 rounded-md bg-red-100 p-2 text-xs text-red-700">
+                {formErrors.phone}
+              </p>
+            )}
           </div>
-          {formErrors?.phone && <p>{formErrors.phone}</p>}
         </div>
 
-        <div>
-          <label>Address</label>
-          <div>
-            <input type="text" name="address" required className="input" />
+        <div className="mb-5 flex flex-col gap-2 sm:flex-row sm:items-center">
+          <label className="sm:basis-40">Address</label>
+          <div className="grow">
+            <input
+              type="text"
+              name="address"
+              required
+              className="input w-full"
+            />
           </div>
         </div>
 
@@ -77,7 +86,9 @@ function CreateOrder() {
             // onChange={(e) => setWithPriority(e.target.checked)}
           />
 
-          <label htmlFor="priority">Want to yo give your order priority?</label>
+          <label className="font-medium" htmlFor="priority">
+            Want to yo give your order priority?
+          </label>
         </div>
 
         <div>
--- front-end/src/ui/Button.jsx
+++ front-end/src/ui/Button.jsx
@@ -2,11 +2,13 @@ import { Link } from "react-router-dom";
 
 const Button = ({ children, disabled, to, type }) => {
   const base =
-    "bg-yellow-400 uppercase font-semibold text-stone-800  inline-block tracking-wide rounded-full hover:bg-yellow-300 transition-colors duration-300 focus:outline-none focus:ring focus:ring-yello-300 focus:bg-yellow-300 focus:ring-offset-2 disabled:cursor-not-allowed";
+    "bg-yellow-400 uppercase text-sm font-semibold text-stone-800  inline-block tracking-wide rounded-full hover:bg-yellow-300 transition-colors duration-300 focus:outline-none focus:ring focus:ring-yellow-300 focus:bg-yellow-300 focus:ring-offset-2 disabled:cursor-not-allowed";
 
   const styles = {
-    primary: base + " px-4 py-3 md:px6 sm:py-4",
+    primary: base + "px-4 py-3 md:px6 sm:py-4",
     small: base + " px-4 py-2 md:px-5 md:py-2.5 text-xs",
+    secondary:
+      "bg-yellow-400 border-2 text-sm border-stone-300  uppercase font-semibold text-stone-400  inline-block tracking-wide rounded-full hover:text-stone-800 hover:bg-stone-300 transition-colors duration-300 focus:outline-none focus:ring focus:ring-stone-200 focus:bg-yellow-300 focus:ring-offset-2 focus:text-stone-800 disabled:cursor-not-allowed px-4 py-2.5 md:px-6 sm:py-3.5",
   };
   if (to)
     return (
