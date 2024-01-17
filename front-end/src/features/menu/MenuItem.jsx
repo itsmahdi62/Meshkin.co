4c670ca25d8e51fcaa4b8d03880c30b36bc5bebc
Mahdi <amiralmasi021@gmail.com>
2024-01-17 22:31:56 +0330

    Add meni items to cart

--- front-end/src/features/menu/MenuItem.jsx
+++ front-end/src/features/menu/MenuItem.jsx
@@ -1,7 +1,22 @@
 import Button from "../../ui/Button";
 import { formatCurrency } from "../../utils/helpers";
+import { useDispatch } from "react-redux";
+import { addItem } from "../cart/cartSlice";
 function MenuItem({ pizza }) {
+  const dispatch = useDispatch();
+
   const { id, name, unitPrice, ingredients, soldOut, imageUrl } = pizza;
+
+  const handleAddToCart = () => {
+    const newItem = {
+      pizzaId: id,
+      name,
+      quantity: 1,
+      unitPrice,
+      totalPrice: unitPrice * 1,
+    };
+    dispatch(addItem(newItem));
+  };
 
   return (
     <li className="flex gap-4 py-2">
@@ -23,9 +38,11 @@ function MenuItem({ pizza }) {
               Sold out
             </p>
           )}
-          <Button className="small" type="small">
-            Add to cart
-          </Button>
+          {!soldOut && (
+            <Button className="small" onClick={handleAddToCart} type="small">
+              Add to cart
+            </Button>
+          )}
         </div>
       </div>
     </li>
--- front-end/src/ui/Button.jsx
+++ front-end/src/ui/Button.jsx
@@ -1,6 +1,6 @@
 import { Link } from "react-router-dom";
 
-const Button = ({ children, disabled, to, type }) => {
+const Button = ({ children, disabled, to, type, onClick }) => {
   const base =
     "bg-yellow-400 uppercase text-sm font-semibold text-stone-800  inline-block tracking-wide rounded-full hover:bg-yellow-300 transition-colors duration-300 focus:outline-none focus:ring focus:ring-yellow-300 focus:bg-yellow-300 focus:ring-offset-2 disabled:cursor-not-allowed";
 
@@ -10,6 +10,12 @@ const Button = ({ children, disabled, to, type }) => {
     secondary:
       "bg-yellow-400 border-2 text-sm border-stone-300  uppercase font-semibold text-stone-400  inline-block tracking-wide rounded-full hover:text-stone-800 hover:bg-stone-300 transition-colors duration-300 focus:outline-none focus:ring focus:ring-stone-200 focus:bg-yellow-300 focus:ring-offset-2 focus:text-stone-800 disabled:cursor-not-allowed px-4 py-2.5 md:px-6 sm:py-3.5",
   };
+  if (onClick)
+    return (
+      <button onClick={onClick} disabled={disabled} className={styles[type]}>
+        {children}
+      </button>
+    );
   if (to)
     return (
       <Link to={to} className={styles[type]}>
