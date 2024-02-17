27114a5d015b803ea1e0eae4386c7cbc683f6072
Mahdi <amiralmasi021@gmail.com>
2024-02-18 00:21:49 +0330

    Create Each product page

--- front-end/src/features/productsList/ProductDetails.js
+++ front-end/src/features/productsList/ProductDetails.js
@@ -1,8 +1,9 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
+import Button from "../../ui/Button";
 const ProductDetails = () => {
   const userId = useParams();
-
+  const [data, setData] = useState();
   useEffect(() => {
     async function getData() {
       const res = await fetch(
@@ -18,14 +19,37 @@ const ProductDetails = () => {
         }
       );
       // if (res.status !== 201) throw Error("Failed getting product");
-      console.log(await res.json());
+      const result = await res.json();
+
+      setData(result.data);
+      console.log(data);
     }
     getData();
   }, []);
   return (
-    <div className="grid grid-cols-3 gap-8 items-center justify-center min-h-screen  border border-red-400 pt-8 ">
-      <div className="rounded-2xl bg-stone-50   min-h-64 border border-red-400 mb-auto"></div>
-      <div className="col-span-2 rounded-2xl bg-stone-50   min-h-64 border border-red-400 mb-auto"></div>
+    <div className="grid grid-cols-3 gap-8 items-center justify-center min-h-screen  pt-8 ">
+      <div className="rounded-2xl bg-stone-50   min-h-96 shadow-sm border border-stone-300 mb-auto flex flex-col justify-center px-12 py-4">
+        {data && <img className="rounded-2xl" src={data.imageURL} alt="" />}
+        <div className="flex my-8">
+          <p>Price</p>
+          {data && <p className="ms-auto">{data.price}$</p>}
+        </div>
+        <Button type="primary">Buy Product</Button>
+      </div>
+      <div className="col-span-2 rounded-2x bg-stone-50 min-h-64 shadow-sm border border-stone-300 mb-auto flex flex-col">
+        {/* what is the program */}
+        <h2 className="text-blue-600">{data && data.title}</h2>
+        <p>{data && data.about}</p>
+        <p className="text-purple-500">{data && data.unlimitedFeatures}</p>
+        {data && data.benefits && (
+          <ul>
+            {data.benefits.map((benefit) => (
+              <li>{benefit}</li>
+            ))}
+          </ul>
+        )}
+        {/* unlimited features*/}
+      </div>
     </div>
   );
 };
