3760aca32861be6865b21c267ceb8a22edf17f9c
Mahdi <amiralmasi021@gmail.com>
2024-03-09 00:02:02 +0330

    Create btc transaction validating

--- back-end/controllers/checkTransactionController.js
+++ back-end/controllers/checkTransactionController.js
@@ -77,51 +77,57 @@ exports.checkTransactionConfirmationTeron = async (req, res, next) => {
   //   console.error("Error checking transaction confirmation:", error);
   // }
 
-  //************************************* invalid apikey */
-  const destinationAddress = "0x5409ed021d9299bf6814279a6a1411a7e866a631"; // replace with the actual destination wallet address
-  const value = req.body.amount; // replace with the actual amount of Ethereum
-  const transactionHash = req.body.hash;
-  const apiUrl = `https://api.etherscan.io/api?module=account&action=txlist&address=${destinationAddress}&value=${value}&sort=desc&page=1&offset=10&startblock=0&endblock=99999999&apikey=B96DUP9C69AIVX2MQFBAZH67ZC7D33W643`;
-  const url = `https://api.etherscan.io/api?module=transaction&action=getstatus&txhash=0xb4bc263278d3f77a652a8d73a6bfd8ec0ba1a63923bbb4f38147fb8a943da26d&apikey=B96DUP9C69AIVX2MQFBAZH67ZC7D33W643`;
-  const URL = `https://api.etherscan.io/api?module=transaction&action=gettxreceiptstatus&txhash=0xb4bc263278d3f77a652a8d73a6bfd8ec0ba1a63923bbb4f38147fb8a943da26d&apikey=B96DUP9C69AIVX2MQFBAZH67ZC7D33W643`;
+  //************************************* correct eth */
+  // const destinationAddress = "0x5409ed021d9299bf6814279a6a1411a7e866a631"; // replace with the actual destination wallet address
+  // const value = req.body.amount;
+  // const transactionHash = req.body.hash;
+  // const apiUrl = `https://api.etherscan.io/api?module=account&action=txlist&address=${destinationAddress}&value=${value}&sort=desc&page=1&offset=10&startblock=0&endblock=99999999&apikey=B96DUP9C69AIVX2MQFBAZH67ZC7D33W643`;
+  // const url = `https://api.etherscan.io/api?module=transaction&action=getstatus&txhash=0xb4bc263278d3f77a652a8d73a6bfd8ec0ba1a63923bbb4f38147fb8a943da26d&apikey=B96DUP9C69AIVX2MQFBAZH67ZC7D33W643`;
+  // const URL = `https://api.etherscan.io/api?module=transaction&action=gettxreceiptstatus&txhash=0xb4bc263278d3f77a652a8d73a6bfd8ec0ba1a63923bbb4f38147fb8a943da26d&apikey=B96DUP9C69AIVX2MQFBAZH67ZC7D33W643`;
 
+  // try {
+  //   const response = await fetch(apiUrl);
+  //   const data = await response.json();
+  //   // console.log(data);
+  //   if (data.result.length !== 0) {
+  //     const transaction = Object.values(data.result).find(
+  //       (tx) => tx.hash === transactionHash && tx.value === value
+  //     );
+  //     if (transaction) {
+  //       console.log(
+  //         `Transaction with hash ${transactionHash} is confirmed with value of ${
+  //           transaction.value / 10 ** 18
+  //         } ETH.`
+  //       );
+  //     } else {
+  //       console.log(
+  //         `Transaction with hash ${transactionHash} is not found or the value is not correct.`
+  //       );
+  //     }
+  //   }
+  // } catch (error) {
+  //   console.error("Error checking transaction confirmation:", error);
+  // }
+  //******************************* btc  */
+  const transactionHash = req.body.hash;
+  const value = req.body.amount;
+  const apiUrl = `https://btcscan.org/api/tx/${transactionHash}/status`;
   try {
-    const response = await fetch(URL);
+    const response = await fetch(apiUrl);
     const data = await response.json();
     console.log(data);
-    if (data.result.length !== 0) {
-      const transaction = Object.values(data.result).find(
-        (tx) => tx.hash === transactionHash 
+    if (data.confirmed) {
+      console.log(
+        `Transaction with hash ${transactionHash} is confirmed with value of ${
+          transaction.value / 10 ** 18
+        } ETH.`
       );
-      if (transaction) {
-        console.log(
-          `Transaction with hash ${transactionHash} is confirmed with value of ${
-            transaction.value / 10 ** 18
-          } ETH.`
-        );
-      } else {
-        console.log(
-          `Transaction with hash ${transactionHash} is not found or the value is not correct.`
-        );
-      }
+    } else {
+      console.log(
+        `Transaction with hash ${transactionHash} is not found or the value is not correct.`
+      );
     }
   } catch (error) {
     console.error("Error checking transaction confirmation:", error);
   }
-  //******************************* */
-  //   const data = {
-  //     jsonrpc: "2.0",
-  //     method: "eth_getTransactionByBlockHashAndIndex",
-  //     params: [req.params.hash, "0x0"],
-  //     id: 64,
-  //   };
-
-  //   fetch("https://api.shasta.trongrid.io/jsonrpc", {
-  //     method: "POST",
-  //     body: JSON.stringify(data),
-  //     headers: { "Content-Type": "application/json" },
-  //   })
-  //     .then((response) => response.json())
-  //     .then((data) => console.log(data))
-  //     .catch((error) => console.error(error));
 };
