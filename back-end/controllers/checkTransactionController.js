750391d1dab69f9bf64f4124b18522c6c066b30f
Mahdi <amiralmasi021@gmail.com>
2024-03-12 19:24:55 +0330

    Add Cardano payment

--- back-end/controllers/checkTransactionController.js
+++ back-end/controllers/checkTransactionController.js
@@ -78,54 +78,82 @@ exports.checkTransactionConfirmationTeron = async (req, res, next) => {
   // }
 
   //************************************* correct eth */
-  // const destinationAddress = "0x5409ed021d9299bf6814279a6a1411a7e866a631"; // replace with the actual destination wallet address
-  // const value = req.body.amount;
-  // const transactionHash = req.body.hash;
-  // const apiUrl = `https://api.etherscan.io/api?module=account&action=txlist&address=${destinationAddress}&value=${value}&sort=desc&page=1&offset=10&startblock=0&endblock=99999999&apikey=B96DUP9C69AIVX2MQFBAZH67ZC7D33W643`;
-  // const url = `https://api.etherscan.io/api?module=transaction&action=getstatus&txhash=0xb4bc263278d3f77a652a8d73a6bfd8ec0ba1a63923bbb4f38147fb8a943da26d&apikey=B96DUP9C69AIVX2MQFBAZH67ZC7D33W643`;
-  // const URL = `https://api.etherscan.io/api?module=transaction&action=gettxreceiptstatus&txhash=0xb4bc263278d3f77a652a8d73a6bfd8ec0ba1a63923bbb4f38147fb8a943da26d&apikey=B96DUP9C69AIVX2MQFBAZH67ZC7D33W643`;
+  //   const destinationAddress = "0x5409ed021d9299bf6814279a6a1411a7e866a631"; // replace with the actual destination wallet address
+  //   const value = req.body.amount;
+  //   const transactionHash = req.body.hash;
+  //  // // const apiUrl = `https://api.etherscan.io/api?module=account&action=txlist&address=${destinationAddress}&value=${value}&sort=desc&page=1&offset=10&startblock=0&endblock=99999999&apikey=B96DUP9C69AIVX2MQFBAZH67ZC7D33W643`;
+  //   //// const url = `https://api.etherscan.io/api?module=transaction&action=getstatus&txhash=0xb4bc263278d3f77a652a8d73a6bfd8ec0ba1a63923bbb4f38147fb8a943da26d&apikey=B96DUP9C69AIVX2MQFBAZH67ZC7D33W643`;
+  //   const URL = `https://api.etherscan.io/api?module=transaction&action=gettxreceiptstatus&txhash=${transactionHash}&apikey=B96DUP9C69AIVX2MQFBAZH67ZC7D33W643`;
 
+  //   try {
+  //     const response = await fetch(URL);
+  //     const data = await response.json();
+  //     // console.log(data);
+  //     if (data.result.length !== 0) {
+  //       const transaction = Object.values(data.result).find(
+  //         (tx) => tx.hash === transactionHash && tx.value === value
+  //       );
+  //       if (transaction) {
+  //         console.log(
+  //           `Transaction with hash ${transactionHash} is confirmed with value of ${
+  //             transaction.value / 10 ** 18
+  //           } ETH.`
+  //         );
+  //       } else {
+  //         console.log(
+  //           `Transaction with hash ${transactionHash} is not found or the value is not correct.`
+  //         );
+  //       }
+  //     }
+  //   } catch (error) {
+  //     console.error("Error checking transaction confirmation:", error);
+  //   }
+  //******************************* teron correct */
+  // const transactionHash = req.body.hash;
+  // const apiUrl = `https://apilist.tronscanapi.com/api/transaction-info?hash=${transactionHash}`;
   // try {
   //   const response = await fetch(apiUrl);
   //   const data = await response.json();
-  //   // console.log(data);
-  //   if (data.result.length !== 0) {
-  //     const transaction = Object.values(data.result).find(
-  //       (tx) => tx.hash === transactionHash && tx.value === value
+  //   // console.log(data.confirmed);
+  //   if (data.confirmed !== "true") {
+  //     console.log(
+  //       `Transaction with hash ${transactionHash} is confirmed with value of ${
+  //         transaction.value / 10 ** 18
+  //       } ETH.`
   //     );
-  //     if (transaction) {
-  //       console.log(
-  //         `Transaction with hash ${transactionHash} is confirmed with value of ${
-  //           transaction.value / 10 ** 18
-  //         } ETH.`
-  //       );
-  //     } else {
-  //       console.log(
-  //         `Transaction with hash ${transactionHash} is not found or the value is not correct.`
-  //       );
-  //     }
+  //   } else {
+  //     console.log(
+  //       `Transaction with hash ${transactionHash} is not found or the value is not correct.`
+  //     );
   //   }
   // } catch (error) {
   //   console.error("Error checking transaction confirmation:", error);
   // }
-  //******************************* teron correct */
-  const transactionHash = req.body.hash;
-  const apiUrl = `https://apilist.tronscanapi.com/api/transaction-info?hash=${transactionHash}`;
+
+  //***************************** */ cardano
+  // const transactionHash = req.body.hash;
+  const transactionHash = "3947f8216288467c7d448479b20cf1c8dc38b45b7c3bdd8ab5623454dfe19140";
+  const apiUrl = `https://api.cardanoscan.io/api/v1/transaction?hash=${transactionHash}`;
   try {
-    const response = await fetch(apiUrl);
-    const data = await response.json();
-    // console.log(data.confirmed);
-    if (data.confirmed !== "true") {
-      console.log(
-        `Transaction with hash ${transactionHash} is confirmed with value of ${
-          transaction.value / 10 ** 18
-        } ETH.`
-      );
-    } else {
-      console.log(
-        `Transaction with hash ${transactionHash} is not found or the value is not correct.`
-      );
-    }
+    const response = await fetch(apiUrl, {
+      headers: {
+        apiKey: "74624597-93a7-4989-ac64-dfb3d4bdfaf4",
+      },
+    });
+    // const data = await response.json();
+    console.log(response);
+    // console.log(response);
+    // if (data.confirmed !== "true") {
+    //   console.log(
+    //     `Transaction with hash ${transactionHash} is confirmed with value of ${
+    //       transaction.value / 10 ** 18
+    //     } ETH.`
+    //   );
+    // } else {
+    //   console.log(
+    //     `Transaction with hash ${transactionHash} is not found or the value is not correct.`
+    //   );
+    // }
   } catch (error) {
     console.error("Error checking transaction confirmation:", error);
   }
