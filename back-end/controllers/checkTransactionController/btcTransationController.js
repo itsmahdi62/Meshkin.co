c0a172d459c994b1a5f27ac6c4aecbed67e5eedb
Mahdi <amiralmasi021@gmail.com>
2024-03-20 18:36:55 +0330

    Complete btc transaction

--- back-end/controllers/checkTransactionController/btcTransationController.js
+++ back-end/controllers/checkTransactionController/btcTransationController.js
@@ -2,39 +2,27 @@ const fetch = require("node-fetch");
 const Order = require("../../models/orderModel");
 exports.btcTransationController = async (req, res, next) => {
   // ********************* btc correct
-  const destinationAddress = "1BvBMSEYstWetqTFn5Au4m4GFg7xJaNVN2"; // replace with the actual destination wallet address
-  const value = 0.001; // replace with the actual amount of Bitcoin
+  const exampleHash =
+    "f4184fc596403b9d638783cf57adfe4c75c605f6356fbc91338530e9831e9e16";
   const transactionHash = req.body.hash;
-  const apiUrl = `https://api.blockchair.com/bitcoin/raw/transaction/${transactionHash}?apikey=YourApiKeyTest`;
-
+  const apiUrl = `https://api.blockchair.com/bitcoin/raw/transaction/${transactionHash}`;
+  const update = { $set: { isPaid: true } };
   try {
     const response = await fetch(apiUrl);
     const data = await response.json();
-    console.log(data);
-    if (data.out && data.out.length > 0) {
-      const transaction = data.out.find(
-        (output) =>
-          output.addr === destinationAddress &&
-          output.value === value * 100000000
-      );
-      if (transaction) {
-        await Order.findByIdAndUpdate(req.params.id, req.body.paid, {
-          new: true,
-          runValidators: true,
-        });
-        res.status(201).json({
-          status: "success",
-        });
-      } else {
-        console.log(
-          `Transaction with hash ${transactionHash} is not found or the value is not correct.`
-        );
-        res.status(404).json({
-          status: "unsuccess",
-        });
-      }
+    // console.log(data);
+    if (data.data) {
+      Order.findOneAndUpdate(data.data, update, { new: true });
+      res.status(201).json({
+        status: "success",
+      });
     } else {
-      console.log("Unable to get transaction list.");
+      console.log(
+        `Transaction with hash ${transactionHash} is not found or the value is not correct.`
+      );
+      res.status(404).json({
+        status: "unsuccess",
+      });
     }
   } catch (error) {
     console.error("Error checking transaction confirmation:", error);
